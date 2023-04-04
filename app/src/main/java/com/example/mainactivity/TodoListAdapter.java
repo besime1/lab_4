aca02b0684bba3e7498b63fdbd3abe0162c90f98
besime1 <bsmdmr86@gmail.com>
2023-04-04 19:37:23 +0530

    TodoListAdapter class created with some method to getitem and count for for list item.

--- app/src/main/java/com/example/mainactivity/TodoListAdapter.java
+++ app/src/main/java/com/example/mainactivity/TodoListAdapter.java
@@ -1,4 +1,36 @@
-package com.example.mainactivity;
+package com.example.androidlabs;
 
-public class TodoListAdapter {
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.mainactivity.TodoList;
+
+import java.util.List;
+
+public class TodoListAdapter extends BaseAdapter {
+
+    List<TodoList> todoLists;
+
+    public TodoListAdapter(List<TodoList> todoLists) {
+        this.todoLists = todoLists;
+    }
+
+    @Override
+    public int getCount() {
+        return todoLists.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return todoLists.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
 }
