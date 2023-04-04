2d2da6ca564600b74eb9e9a54ab3f1cdcd02b443
besime1 <bsmdmr86@gmail.com>
2023-04-04 19:50:37 +0530

    onlongitem click listener applied on list item and code written to create alert dialog

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mainactivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Switch;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
@@ -26,5 +30,32 @@ public class MainActivity extends AppCompatActivity {
         List<TodoList> todoList = appDatabase.getAllTasks();
         TodoListAdapter todoAdapter = new TodoListAdapter(todoList);
         listView.setAdapter(todoAdapter);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.do_you_want_to_delete);
+                int rowNo= position+1;
+                builder.setMessage(getString(R.string.selected_row)+rowNo);
+                builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        appDatabase.delete(todoList.get(position));
+                        todoAdapter.removeItem(position);
+
+
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                return true;
+            }
+        });
     }
 }
