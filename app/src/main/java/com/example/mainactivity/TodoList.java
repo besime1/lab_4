3fed5590e3d7b0c401074b074d9deaf474f8dee8
besime1 <bsmdmr86@gmail.com>
2023-04-04 19:27:53 +0530

    TodoList class created

---
+++ app/src/main/java/com/example/mainactivity/TodoList.java
@@ -0,0 +1,41 @@
+package com.example.mainactivity;
+
+public class TodoList {
+    int _id;
+    String name;
+    Boolean isUrgent;
+
+    public TodoList(int _id, String name, Boolean isUrgent) {
+        this._id = _id;
+        this.name = name;
+        this.isUrgent = isUrgent;
+    }
+
+    public TodoList() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getUrgent() {
+        return isUrgent;
+    }
+
+    public void setUrgent(Boolean urgent) {
+        isUrgent = urgent;
+    }
+
+    public int getID() {
+        return _id;
+    }
+
+    public void setId(int _id) {
+        this._id = _id;
+    }
+}
