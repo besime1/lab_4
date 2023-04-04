ed628747a6a397495df17afd8431d568bc081abc
besime1 <bsmdmr86@gmail.com>
2023-04-04 19:44:15 +0530

    class created to implement database in application

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,94 +1,9 @@
 package com.example.mainactivity;
-import static java.security.AccessController.getContext;
-
-import android.annotation.SuppressLint;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<String> items;
-     private ArrayAdapter<String> itemsAdapter;
-    private ListView listView;
-    private Button button;
 
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected  void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        listView= findViewById(R.id.listView);
-        button=findViewById(R.id.button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                addItem(view);
-            }
-        });
-
-        items= new ArrayList<>();
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-        listView.setAdapter(itemsAdapter);
-        setUpListVievListener();
-
-
-
-    }
-
-    private void setUpListVievListener() {
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Context context =getApplicationContext();
-                Toast.makeText(context, "Item Removed", Toast.LENGTH_SHORT).show();
-                items.remove(i);
-                itemsAdapter.notifyDataSetChanged();
-                return true;
-
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(getContext());
-                AlertDialog.Builder builder;
-                builder.setTitle("Do you want to delete this?");
-                String position;
-                builder.setMessage("The selected row is: " + position);
-                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // delete the selected row
-                    }
-                });
-
-                builder.setNegativeButton("No", null);
-
-            }
-        });
-    }
-
-    private void addItem(View view) {
-        EditText input = findViewById(R.id.editText2);
-        String item = input.getText().toString();
-
-        String itemText = null;
-        if(!(itemText.equals(""))){
-            itemsAdapter.add(itemText);
-            input.setText("");
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "Please enter text..", Toast.LENGTH_LONG).show();
-        }
-
-    }
 }
--- app/src/main/java/com/example/mainactivity/TodoDatabase.java
+++ app/src/main/java/com/example/mainactivity/TodoDatabase.java
@@ -1,4 +1,89 @@
 package com.example.mainactivity;
 
-public class TodoDatabase {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoDatabase extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "todomanager";
+    private static final String TABLE_TODO = "todotable";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_IS_URGENT = "isUrgent";
+
+    public TodoDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_TODO + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_IS_URGENT + " BOOLEAN" + ")";
+        db.execSQL(CREATE_CONTACTS_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TODO);
+        onCreate(db);
+    }
+
+    void addTodo(TodoList contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, contact.getName());
+        values.put(KEY_IS_URGENT, contact.getUrgent());
+        db.insert(TABLE_TODO, null, values);
+        db.close();
+    }
+
+    public List<TodoList> getAllTasks() {
+        List<TodoList> contactList = new ArrayList<TodoList>();
+        // Select All Query
+
+        String selectQuery = "SELECT  * FROM " + TABLE_TODO;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Log.w("Data Base Version:", String.valueOf(db.getVersion()));
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        printCursor(cursor);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                TodoList todo = new TodoList();
+                todo.setId(Integer.parseInt(cursor.getString(0)));
+                todo.setName(cursor.getString(1));
+                boolean isUrgent = false;
+                if (Integer.parseInt(cursor.getString(2)) == 1) {
+                    isUrgent = true;
+                }
+                todo.setUrgent(isUrgent);
+                contactList.add(todo);
+            } while (cursor.moveToNext());
+        }
+        return contactList;
+    }
+
+    void printCursor(Cursor c) {
+        Log.w("Number of columns", String.valueOf(c.getColumnCount()));
+        Log.w("Columns 0 Name", c.getColumnName(0));
+        Log.w("Columns 1 Name", c.getColumnName(1));
+        Log.w("Columns 2 Name", c.getColumnName(2));
+        Log.w("Number of Records", String.valueOf(c.getCount()));
+        Log.w("Number of Rows", String.valueOf(c.getCount()));
+
+
+    }
+
+
 }
